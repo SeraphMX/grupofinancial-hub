3159549969dde186d44b09f06c79eced282e24dd
Seraph <seraph.axl@gmail.com>
2025-02-22 11:28:40 -0600

    Agrega configuraciones de Tailwind CSS, incluyendo nuevos colores, animaciones y fuentes; también se añade el plugin SVGR en Vite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueFinancial: '#003366' // Navy blue from logo
+        //secondary: '#0088FF' // Light blue from logo
+      },
+      animation: {
+        'slide-up': 'slideUp 0.5s ease-out'
+      },
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' }
+        }
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        sans: ['"Open Sans"', 'sans-serif']
+      }
+    }
   },
-  darkMode: "class",
-  plugins: [nextui()],
-};
+  darkMode: 'class',
+  plugins: [nextui()]
+}
