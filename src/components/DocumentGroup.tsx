35e7d3114c46e541f13080d9478c00bbeb22189d
Seraph <seraph.axl@gmail.com>
2025-02-27 11:53:45 -0600

    Refactoriza el componente DocumentGroup para manejar documentos con múltiples archivos y mejora la visualización de estados de documentos

--- src/components/DocumentGroup.tsx
+++ src/components/DocumentGroup.tsx
@@ -1,59 +1,8 @@
 import { Button, Card, Chip, Tooltip } from '@nextui-org/react'
-import { format } from 'date-fns'
-import { es } from 'date-fns/locale'
-import {
-  AlertTriangle,
-  Check,
-  CheckCircle2,
-  ChevronDown,
-  ChevronRight,
-  CloudUpload,
-  Eye,
-  FileCheck,
-  FileMinus2,
-  FilePlus2,
-  FileText,
-  FileWarning,
-  FileX,
-  X
-} from 'lucide-react'
+import { ChevronDown, ChevronRight, CloudUpload, FileCheck, FileMinus2, FilePlus2, FileText, FileWarning, FileX } from 'lucide-react'
 import { useState } from 'react'
 import { Document } from '../schemas/documentSchemas'
-
-const DocumentStatus = ({ status }: { status: 'pendiente' | 'aceptado' | 'rechazado' | 'excluido' }) => {
-  const statusConfig = {
-    pendiente: {
-      color: 'warning',
-      icon: AlertTriangle,
-      text: 'En revisión'
-    },
-    aceptado: {
-      color: 'success',
-      icon: CheckCircle2,
-      text: 'Aceptado'
-    },
-    rechazado: {
-      color: 'danger',
-      icon: FileX,
-      text: 'Rechazado'
-    },
-    excluido: {
-      color: 'primary',
-      icon: FileX,
-      text: 'Excluido'
-    }
-  } as const
-
-  const config = statusConfig[status]
-
-  return (
-    <div className='flex items-center gap-3'>
-      <Chip variant='flat' color={config.color}>
-        {config.text}
-      </Chip>
-    </div>
-  )
-}
+import DocumentFile from './DocumentFile'
 
 const DocumentGroup = ({
   title,
@@ -62,7 +11,8 @@ const DocumentGroup = ({
   onAccept,
   onReject,
   onExclude,
-  onInclude
+  onInclude,
+  allDocuments
 }: {
   title: string
   documents: Document[]
@@ -71,8 +21,29 @@ const DocumentGroup = ({
   onReject: (doc: Document) => void
   onExclude: (doc: Document) => void
   onInclude: (doc: Document) => void
+  allDocuments: any[]
 }) => {
   const [isExpanded, setIsExpanded] = useState(true)
+
+  // Función para verificar si un documento tiene múltiples archivos cargados
+  const hasMultipleFiles = (doc: Document) => {
+    return doc.multipleFiles && doc.dbDocument?.status === 'pendiente'
+  }
+
+  // Función para obtener todos los documentos de un tipo específico
+  const getDocumentFiles = (doc: Document) => {
+    if (!doc.multipleFiles) return []
+
+    //Trae los documentos del mismo tipo que el documento actual y que no estén pendientes
+    return allDocuments.filter((d) => d.tipo === doc.name).filter((d) => d.status !== 'pendiente')
+    //.sort((a, b) => (a.status || '').localeCompare(b.status || ''))
+  }
+
+  // Verificar si un documento tiene múltiples archivos subidos
+  const hasMultipleUploads = (doc: Document) => {
+    const files = getDocumentFiles(doc)
+    return doc.multipleFiles && files.length > 0
+  }
 
   return (
     <div className='space-y-2'>
@@ -89,19 +60,18 @@ const DocumentGroup = ({
           {documents.map((doc) => (
             <Card key={doc.id} className='p-4'>
               <div className='flex flex-col sm:flex-row sm:items-center justify-between'>
-                <div className='flex sm:items-center gap-2 sm:gap-4'>
+                <div className='flex sm:items-center gap-2 sm:gap-4 w-full'>
                   {doc.dbDocument?.status === 'aceptado' ? (
                     <FileCheck className='text-success' size={24} />
                   ) : doc.dbDocument?.status === 'rechazado' ? (
                     <FileX className='text-danger' size={24} />
-                  ) : doc.dbDocument?.status === 'pendiente' ? (
-                    // Caso por defecto: pendiente
+                  ) : doc.dbDocument?.status === 'revision' ? (
                     <FileWarning className='text-warning' size={24} />
                   ) : (
-                    // Caso por defecto
+                    // Caso por defecto: pendiente
                     <FileText className='text-primary' size={24} />
                   )}
-                  <div>
+                  <div className='flex-grow'>
                     <h3 className='text-medium font-semibold flex justify-between sm:justify-start items-center gap-2'>
                       {doc.name}
                       {doc.required && (
@@ -111,67 +81,76 @@ const DocumentGroup = ({
                       )}
                     </h3>
                     <p className='text-small text-default-500 pt-4 sm:pt-1'>{doc.description}</p>
-                    {doc.dbDocument && (
-                      <div className='mt-2 space-y-2'>
-                        {doc.dbDocument.status !== 'excluido' && (
-                          <div className='flex items-center gap-2 text-tiny text-default-400'>
-                            Archivo:
-                            <Tooltip
-                              content={
-                                <div className='text-left'>
-                                  <p>Cargado el: {format(new Date(doc.dbDocument.created_at), 'd MMM yyyy, HH:mm', { locale: es })}</p>
-                                  {doc.dbDocument.file_size && <p>Tamaño: {(doc.dbDocument.file_size / 1024 / 1024).toFixed(2)} MB</p>}
-                                </div>
-                              }
-                            >
-                              {doc.dbDocument.original_name}
-                            </Tooltip>
-                          </div>
-                        )}
 
-                        <DocumentStatus status={doc.dbDocument.status} />
+                    {/* Mostrar un solo documento si no es multipleFiles */}
+                    {doc.dbDocument &&
+                      !doc.multipleFiles &&
+                      (doc.dbDocument.status === 'excluido' ? (
+                        <Chip variant='flat' color='primary'>
+                          Documento excluido
+                        </Chip>
+                      ) : (
+                        <div className='space-y-1 mt-3'>
+                          <p className='text-small font-medium'>Archivo cargado:</p>
+                          <div className='max-h-60 overflow-y-auto space-y-1 p-2 border rounded-lg border-default-100'>
+                            <DocumentFile
+                              key={doc.dbDocument.id}
+                              file={doc.dbDocument}
+                              onView={() => {
+                                const docWithFile = { ...doc, dbDocument: doc.dbDocument }
+                                onView(docWithFile)
+                              }}
+                              onAccept={() => {
+                                const docWithFile = { ...doc, dbDocument: doc.dbDocument }
+                                onAccept(docWithFile)
+                              }}
+                              onReject={() => {
+                                const docWithFile = { ...doc, dbDocument: doc.dbDocument }
+                                onReject(docWithFile)
+                              }}
+                            />
+                          </div>
+                        </div>
+                      ))}
+
+                    {/* Mostrar múltiples documentos si es multipleFiles */}
+                    {doc.multipleFiles && (
+                      <div className='mt-2'>
+                        {hasMultipleUploads(doc) &&
+                          (doc.dbDocument?.status === 'excluido' ? (
+                            <Chip variant='flat' color='primary'>
+                              Documento excluido
+                            </Chip>
+                          ) : (
+                            <div className='space-y-1 mt-3'>
+                              <p className='text-small font-medium'>Archivos cargados:</p>
+                              <div className='max-h-60 overflow-y-auto space-y-1 p-2 border rounded-lg border-default-100'>
+                                {getDocumentFiles(doc).map((file) => (
+                                  <DocumentFile
+                                    key={file.id}
+                                    file={file}
+                                    onView={() => {
+                                      const docWithFile = { ...doc, dbDocument: file }
+                                      onView(docWithFile)
+                                    }}
+                                    onAccept={() => {
+                                      const docWithFile = { ...doc, dbDocument: file }
+                                      onAccept(docWithFile)
+                                    }}
+                                    onReject={() => {
+                                      const docWithFile = { ...doc, dbDocument: file }
+                                      onReject(docWithFile)
+                                    }}
+                                  />
+                                ))}
+                              </div>
+                            </div>
+                          ))}
                       </div>
                     )}
                   </div>
                 </div>
                 <div className='flex items-center self-end mt-4 sm:m-0 sm:self-auto gap-2'>
-                  {doc.dbDocument && doc.dbDocument.status !== 'excluido' && doc.dbDocument.status !== 'rechazado' && (
-                    <>
-                      <Tooltip content='Ver documento'>
-                        <Button isIconOnly color='primary' variant='ghost' onPress={() => onView(doc)}>
-                          <Eye />
-                        </Button>
-                      </Tooltip>
-                    </>
-                  )}
-
-                  {doc.dbDocument && doc.dbDocument.status === 'pendiente' && (
-                    <>
-                      <Tooltip content='Aceptar documento'>
-                        <Button
-                          color='success'
-                          variant='ghost'
-                          isIconOnly
-                          //isLoading={processingDoc === doc.id}
-                          onPress={() => onAccept(doc)}
-                        >
-                          <Check />
-                        </Button>
-                      </Tooltip>
-                      <Tooltip content='Rechazar documento'>
-                        <Button
-                          color='danger'
-                          variant='ghost'
-                          isIconOnly
-                          //isLoading={processingDoc === doc.id}
-                          onPress={() => onReject(doc)}
-                        >
-                          <X />
-                        </Button>
-                      </Tooltip>
-                    </>
-                  )}
-
                   {!doc.dbDocument && !doc.required && (
                     <>
                       <Tooltip content='Excluir documento'>
