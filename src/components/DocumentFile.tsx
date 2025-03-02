0ffb2cc8cb822232270901ca84445a8078ef36b8
Seraph <seraph.axl@gmail.com>
2025-03-02 05:10:12 -0600

    Mejora el componente DocumentFile: agrega tooltip a botones y actualiza íconos para acciones de archivo

--- src/components/DocumentFile.tsx
+++ src/components/DocumentFile.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@nextui-org/react'
-import { Check, Eye, FileText, X } from 'lucide-react'
+import { Button, Tooltip } from '@nextui-org/react'
+import { CheckCircle, CircleOff, FileSearch, FileText, Trash } from 'lucide-react'
 import { useSelector } from 'react-redux'
-import DocumentStatus from '../pages/DocumentStatus'
+import DocumentStatus from './DocumentStatus'
 
 // Componente para mostrar un archivo individual
 const DocumentFile = ({
@@ -20,45 +20,44 @@ const DocumentFile = ({
   const isAdmin = Boolean(useSelector((state: any) => state.auth?.user?.role) === 'admin')
 
   return (
-    <div className='flex justify-between items-center p-2 bg-default-100 rounded-lg my-1'>
-      <div className='flex items-center gap-2 overflow-hidden'>
+    <div className='flex flex-col sm:flex-row gap-2 justify-between sm:items-center p-2 bg-default-100 rounded-lg my-1'>
+      <div className='flex items-center gap-2 overflow-hidden justify-between sm:justify-start'>
         <FileText size={16} className='flex-shrink-0 text-primary' />
-        <span className='text-small truncate'>{file.original_name}</span>
-        {file.file_size && (
-          <span className='text-tiny text-default-500 flex-shrink-0'>({(file.file_size / 1024 / 1024).toFixed(2)} MB)</span>
-        )}
+        <span className='text-small truncate flex-grow'>{file.original_name}</span>
+        {file.file_size && <span className='text-tiny text-default-500 flex-none'>({(file.file_size / 1024 / 1024).toFixed(2)} MB)</span>}
       </div>
-      <div className='flex items-center gap-1'>
-        <DocumentStatus status={file.status} rejectCause={file.reject_cause} />
-        {file.status !== 'rechazado' && (
-          <Button isIconOnly size='sm' color='primary' variant='light' onPress={onView}>
-            <Eye size={16} />
-          </Button>
-        )}
+      <div className='items-center gap-1 flex flex-wrap sm:flex-nowrap justify-between'>
+        <DocumentStatus status={file.status} rejectCause={file.reject_cause} onReject={onReject} />
 
-        {file.status === 'pendiente' && (
-          <Button isIconOnly size='sm' color='danger' variant='light' onPress={onDelete}>
-            <X size={16} />
-          </Button>
-        )}
-
-        {/*TODO:Verificar sin necesitamos este boton , y por que al eliminar de la base no recarga */}
-        {file.status === 'rechazado' && (
-          <Button isIconOnly size='sm' color='danger' variant='light' onPress={onDelete}>
-            <X size={16} />
-          </Button>
-        )}
+        {file.status === 'rechazado' ||
+          (file.status === 'pendiente' && (
+            <Tooltip content='Eliminar archivo' placement='top'>
+              <Button isIconOnly size='sm' color='danger' variant='light' onPress={onDelete}>
+                <Trash />
+              </Button>
+            </Tooltip>
+          ))}
 
         {isAdmin && file.status === 'revision' && (
           <>
-            <Button isIconOnly size='sm' color='success' variant='light' onPress={onAccept}>
-              <Check size={16} />
-            </Button>
-            <Button isIconOnly size='sm' color='danger' variant='light' onPress={onReject}>
-              <X size={16} />
-            </Button>
+            <Tooltip content='Rechazar' placement='top'>
+              <Button isIconOnly size='sm' color='danger' variant='light' onPress={onReject}>
+                <CircleOff />
+              </Button>
+            </Tooltip>
+            <Tooltip content='Aceptar' placement='top'>
+              <Button isIconOnly size='sm' color='success' variant='light' onPress={onAccept}>
+                <CheckCircle />
+              </Button>
+            </Tooltip>
           </>
         )}
+
+        <Tooltip content='Ver archivo' placement='top'>
+          <Button isIconOnly size='sm' color='primary' variant='light' onPress={onView}>
+            <FileSearch />
+          </Button>
+        </Tooltip>
       </div>
     </div>
   )
